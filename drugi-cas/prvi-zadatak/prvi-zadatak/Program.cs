7385aa73c391ae360564f7e7823474d6e2458286
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-11-01 20:05:02 +0100

    PrviZadatak/DrugiDan

---
+++ drugi-cas/prvi-zadatak/prvi-zadatak/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prvi_zadatak
+{
+    class Inkvizicija
+    {
+        public static void PlusPlusDva(ref int x)
+        {
+            x += 2;
+        }
+
+        public static void Namjesti(ref int x, ref int y)
+        {
+            y = x + 2;
+        }
+
+        public static void Srednja(IEnumerable<int> values, out int median)
+        {
+            int sum = 0;
+            int count = values.Count();
+            foreach(int x in values)
+            {
+                sum += x;
+            }
+            median = (int)Math.Round((double)sum / count , 0);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metod 1");
+            int x = 3;
+            Console.WriteLine("x : " + x);
+            Inkvizicija.PlusPlusDva(ref x);
+            Console.WriteLine("x : " + x + '\n');
+            x = 5;
+            int y = 4;
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+            Inkvizicija.Namjesti(ref x, ref y);
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y + '\n');
+
+            int[] niz = { 4, 5, 10, 12 };
+            int output;
+            Console.WriteLine("niz : ");
+            foreach (int i in niz)
+            {
+                Console.WriteLine(i);
+            }
+
+            Inkvizicija.Srednja(niz, out output);
+            Console.WriteLine("median : " + output);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ drugi-cas/prvi-zadatak/prvi-zadatak/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("prvi-zadatak")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("prvi-zadatak")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("659861ed-a6ab-4b95-88b4-99bc7511f5d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
