cec2659d5d1d65345de6a47f671aa79994942a5b
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-11-01 20:15:18 +0100

    DrugiCas/DrugiZadatak

---
+++ drugi-cas/drugiZadatak/drugiZadatak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drugiZadatak
+{
+    class KompleksanBroj
+    {
+        private int realanDeo, imaginarniDeo;
+
+        public KompleksanBroj(int realanDeo, int imaginarniDeo)
+        {
+            this.realanDeo = realanDeo;
+            this.imaginarniDeo = imaginarniDeo;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KompleksanBroj broj = new KompleksanBroj(3, 4);
+        }
+    }
+}
