238364c82da35be2a119c6f9e10ac8ca4fbf9f58
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-11-15 20:27:40 +0100

    Racun

---
+++ cetvrti-cas/Rachuuun/Rachuuun/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rachuuun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Racun racun = new Racun("SubscribeToPewdiepie", "Kek", 1000, -1000);
+            char input = (char)0;
+
+            do
+            {
+                Console.WriteLine("_________RACUN________");
+                Console.WriteLine("Q - Ispisi balans");
+                Console.WriteLine("W - Dodaj na balans");
+                Console.WriteLine("E - Skini sa balansa");
+                Console.WriteLine("X - Izadji iz programa");
+                Console.Write("> ");
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (input)
+                {
+                    case 'Q':
+                    case 'q':
+                        Console.WriteLine("Balans : " + racun.Sredstva);
+                        break;
+
+                    case 'W':
+                    case 'w':
+                        Console.WriteLine("Unesite koliko da dodamo");
+                        Console.Write("> ");
+                        try
+                        {
+                            double balans = Convert.ToDouble(Console.ReadLine());
+                            if (racun.DodajBalans(balans))
+                            {
+                                Console.WriteLine("Uspesno dodat balans.");
+                            }
+                            else
+                            {
+                                throw new Exception("Nismo uspeli da dodamo na balans.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case 'E':
+                    case 'e':
+                        Console.WriteLine("Unesite koliko da skinemo");
+                        Console.Write("> ");
+                        try
+                        {
+                            double balans = Convert.ToDouble(Console.ReadLine());
+                            if (racun.PodigniNovac(balans))
+                            {
+                                Console.WriteLine("Uspesno skinut balans.");
+                            }
+                            else
+                            {
+                                throw new Exception("Nismo uspeli da skinemo sa balansa.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case 'X':
+                    case 'x':
+                        // nista se ne desava, do while ce nas izbaciti napolje
+                        break;
+
+                    default:
+                        Console.WriteLine("Uneli ste opciju koja ne postoji.");
+                        break;
+                }
+            }
+            while (input != 'X' && input != 'x');
+        }
+    }
+}
---
+++ cetvrti-cas/Rachuuun/Rachuuun/Racun.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rachuuun
+{
+    public class Racun
+    {
+        string brojRacuna;
+        string imeKorisnika;
+        double sredstva;
+        double dozvoljeniMinus;
+
+        public string BrojRacuna { get => brojRacuna; }
+        public string ImeKorisnika { get => imeKorisnika; }
+        public double Sredstva { get => sredstva; }
+        public double DozvoljeniMinus { get => dozvoljeniMinus; }
+
+        public Racun(string brojRacuna, string imeKorisnika, double sredstva, double dozvoljeniMinus)
+        {
+            this.brojRacuna = brojRacuna;
+            this.imeKorisnika = imeKorisnika;
+            this.sredstva = sredstva;
+            this.dozvoljeniMinus = dozvoljeniMinus;
+        }
+
+        public bool DodajBalans(double balans)
+        {
+            try
+            {
+                sredstva += balans;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool PodigniNovac(double novac)
+        {
+            try
+            {
+                if (sredstva - novac < dozvoljeniMinus)
+                {
+                    return false;
+                }
+                else
+                {
+                    sredstva -= novac;
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
