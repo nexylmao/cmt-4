1aa29928bdd412b82eb3241bce88267b471e4e26
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2019-01-18 21:03:08 +0100

    Priprema za kontrolni

---
+++ MySerializer/MySerializer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MySerializer
+{
+    class Program
+    {
+        static void Serialize<T>(T obj, string fileName)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            TextWriter tw = new StreamWriter(fileName);
+            xs.Serialize(tw, obj);
+            tw.Close();
+        }
+
+        static T Deserialize<T>(string fileName)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            TextReader tr = new StreamReader(fileName);
+            return (T)xs.Deserialize(tr);
+        }
+
+        static void Main(string[] args)
+        {
+            Student st = new Student("Nenad", "Vuletic", "DONTYOUF", new DateTime(2000, 8, 19));
+            Serialize(st, "Nenad.xml");
+            Student st2 = Deserialize<Student>("Nenad.xml");
+            Console.WriteLine(st2);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ MySerializer/MySerializer/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MySerializer
+{
+    public class Student
+    {
+        string name;
+        string familyName;
+        string jmbg;
+        DateTime birthday;
+
+        public Student() { }
+
+        public Student(string name, string familyName, string jmbg, DateTime birthday)
+        {
+            this.name = name;
+            this.familyName = familyName;
+            this.jmbg = jmbg;
+            this.birthday = birthday;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string FamilyName { get => familyName; set => familyName = value; }
+        public string JMBG { get => jmbg; set => jmbg = value; }
+        public DateTime Birthday { get => birthday; set => birthday = value; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
