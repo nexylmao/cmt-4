693e659bba1be543e7c0ad4210f4577e98612345
Nenad Vuletic <nenadvvvvvvv@gmail.com>
2019-06-04 13:29:49 +0200

    Ispravak kontrolnog

---
+++ Ispravak-ADO.NET/Zadatak1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Zadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(
+                    "Data Source=CMTLAB2019\\SQLEXPRESS;" +
+                    "Integrated Security=SSPI;" +
+                    "Initial Catalog=CMT"))
+                {
+                    connection.Open();
+
+                    var query = "SELECT ime, prezime, razred, s.naziv as skola FROM polaznik p\n" +
+                                    "INNER JOIN (SELECT * FROM pohadja WHERE ocena > 3) po ON p.polaznikId = po.polaznikId\n" +
+                                    "INNER JOIN (SELECT * FROM Kurs WHERE naziv = \'P-K4-ISA\') k ON k.kursId = po.kursId\n" +
+                                    "INNER JOIN skola s ON p.skolaId = s.skolaId\n" +
+                                    "ORDER BY p.ime, p.prezime ASC;";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+
+                            Console.WriteLine("Redni broj | {0} | {1} | {2} | {3} |", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3));
+                            Console.WriteLine();
+                            int i = 1;
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{4} | {0} | {1} | {2} | {3} |", reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), i);
+                                i++;
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    Console.ReadKey();
+                }
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Ispravak-ADO.NET/Zadatak2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceKurs = "P-K3-OOP";
+            var targetKurs = "P-K4-ISA";
+
+            using (SqlConnection connection = new SqlConnection(
+                    "Data Source=CMTLAB2019\\SQLEXPRESS;" +
+                    "Integrated Security=SSPI;" +
+                    "Initial Catalog=CMT"))
+            using (DataSet dataSet = new DataSet())
+            {
+                var selectPolaznik = "SELECT * FROM polaznik p;";
+                var selectPohadja = "SELECT * FROM pohadja po;";
+                var selectKurs = "SELECT * FROM kurs k;";
+
+                using (SqlDataAdapter polaznikAdapter = new SqlDataAdapter(selectPolaznik, connection),
+                    pohadjaAdapter = new SqlDataAdapter(selectPohadja, connection),
+                    kursAdapter = new SqlDataAdapter(selectKurs, connection))
+                {
+                    var insertPohadja = "INSERT pohadja (polaznikId, kursId) VALUES (@polaznikId, @kursId);";
+                    var insertPohadjaCommand = new SqlCommand(insertPohadja, connection);
+                    var parameter = insertPohadjaCommand.Parameters.Add("@polaznikId", SqlDbType.Int);
+                    parameter.SourceColumn = "polaznikId";
+                    parameter = insertPohadjaCommand.Parameters.Add("@kursId", SqlDbType.Int);
+                    parameter.SourceColumn = "kursId";
+
+                    polaznikAdapter.Fill(dataSet, "Polaznik");
+                    pohadjaAdapter.Fill(dataSet, "Pohadja");
+                    pohadjaAdapter.InsertCommand = insertPohadjaCommand;
+                    kursAdapter.Fill(dataSet, "Kurs");
+
+                    // Uzimanje ID kurs-a sa imenom sourceKurs, targetKurs
+                    var row = dataSet.Tables["Kurs"].Select($"naziv = \'{sourceKurs}\'")[0];
+                    var sourceKursId = row["kursId"];
+                    row = dataSet.Tables["Kurs"].Select($"naziv = \'{targetKurs}\'")[0];
+                    var targetKursId = row["kursId"];
+
+                    // Polaznici koji idu na kurs
+                    var polazniciIds = dataSet.Tables["Pohadja"].Select($"kursId = {sourceKursId}");
+                    Console.WriteLine($"Ucenici koji pohadjaju kurs {sourceKurs} : ");
+                    foreach (var polaznikId in polazniciIds)
+                    {
+                        // ISPIS
+                        var polaznik = dataSet.Tables["Polaznik"].Select($"polaznikId = {polaznikId["polaznikId"]}")[0];
+                        Console.WriteLine("{0} {1}", polaznik["Ime"], polaznik["Prezime"]);
+
+                        // DODAVANJE
+                        var noviPolaznik = dataSet.Tables["Pohadja"].NewRow();
+                        noviPolaznik["polaznikId"] = polaznikId["polaznikId"];
+                        noviPolaznik["kursId"] = targetKursId;
+                        dataSet.Tables["Pohadja"].Rows.Add(noviPolaznik);
+                        Console.WriteLine($"Korisnik je upisan na kurs {targetKurs}");
+                    }
+
+                    pohadjaAdapter.Update(dataSet.Tables["Pohadja"]);    
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
